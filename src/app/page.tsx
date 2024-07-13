5e36fd6c826e51abfb645db2557f6ce8b57958a3
David Doria <daviddoria@Davids-M1-MacBook-Pro.local>
2024-07-13 09:53:28 -0700

    use next/image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getMyImages } from "~/server/queries";
 
@@ -7,10 +8,16 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} alt="image" />
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+          <Image
+            src={image.url}
+            // style={{ objectFit: "contain" }}
+            width={480}
+            height={480}
+            alt={image.name}
+          />
           <div>{image.name}</div>
         </div>
       ))}
