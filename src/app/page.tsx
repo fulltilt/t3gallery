c2ff29617105b71b4aa0715bcf10a2fada9bbc1f
David Doria <daviddoria@Davids-MacBook-Pro-2.local>
2024-07-12 21:52:05 -0700

    setup initial mock images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/ada6bfbb-6e3c-4522-91e8-a35f51d3fe20-7ryamc.png",
+  "https://utfs.io/f/32693d40-1703-43af-82bd-d78994d8c599-sy3n9e.png",
+  "https://utfs.io/f/b4a6318f-dfb7-4382-b71f-589749ae7177-vbzg9s.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
